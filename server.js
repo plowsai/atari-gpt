2a8009f061efb5c0510c68448da931a57f40dd89
Jordan Plows <{ID}+{username}@users.noreply.github.com>
2023-03-14 19:00:48 -0700

    chore: commit save point

--- server.js
+++ server.js
@@ -1,47 +1,49 @@
-
-const express = require('express')
-// const bodyParser = require('body-parser')
-// const session = require('express-session')
-// const path = require('path')
-
-// const dev = process.env.NODE_ENV !== 'production'
-// const next = require('next')
-// const pathMatch = require('path-match')
-// const app = next({ dev })
-// const handle = app.getRequestHandler()
-// const route = pathMatch()
-// const {parse} = require('url')
-
-const app = express()
-// const apiRoutes = require ('./routes/api')
-const port = 3000
-
-
-app.set("view engine", "ejs")
-
-app.get('/', (req, res)=> {
-    res.render("index.ejs", {
-    })
-})
-
-app.get("/changelog", (req, res) => {
-    res.render("changelog.ejs")
- })
-// app.get("/desktop-ai", (req, res) => {
-//     res.render("desktop-ai.ejs")
-//  })
-// app.get("/browser-ai", (req, res) => {
-//     res.render("browser-ai.ejs")
-//  })
-app.get("/early-access", (req, res) => {
-    res.render("early-access.ejs")
- })
-app.get("/terms-of-service", (req, res) => {
-    res.render("terms.ejs")
- })
-
-app.use(express.static('public'))
-
-app.listen(port, () => {
-  console.log(`Listening on http://localhost:${port}`)
-})
+// Define your routes
+const routes = {
+    '/pacman/index.html': 'pacman/index.html',
+    '/pong/index.html': 'pong/index.html',
+    '/invaders': 'invaders/index.html',
+  };
+  
+  // Set up the HTML structure
+  const header = document.querySelector('header');
+  const footer = document.querySelector('footer');
+  const container = document.querySelector('#content');
+  
+  // Load the initial page
+  loadPage(window.location.pathname);
+  
+  // Listen for changes in the browser's URL
+  window.addEventListener('popstate', () => {
+    loadPage(window.location.pathname);
+  });
+  
+  // Function to load a page
+  function loadPage(pathname) {
+    // Get the corresponding HTML file from the routes object
+    const filename = routes[pathname];
+    if (!filename) return;
+  
+    // Load the HTML file and insert it into the container element
+    fetch(filename)
+      .then(response => response.text())
+      .then(html => {
+        header.innerHTML = getHeader(html);
+        container.innerHTML = getContent(html);
+        footer.innerHTML = getFooter(html);
+      });
+  }
+  
+  // Helper functions to extract header, content, and footer from the HTML file
+  function getHeader(html) {
+    return html.match(/<header>([\s\S]*?)<\/header>/i)[0];
+  }
+  
+  function getContent(html) {
+    return html.match(/<main>([\s\S]*?)<\/main>/i)[0];
+  }
+  
+  function getFooter(html) {
+    return html.match(/<footer>([\s\S]*?)<\/footer>/i)[0];
+  }
+  
