e06abe3426715eb3c4e58068fe31c90771b467c3
Jordan Plows <{ID}+{username}@users.noreply.github.com>
2023-03-14 21:44:14 -0700

    chore: commit save point

--- server.js
+++ server.js
@@ -3,31 +3,47 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files with custom headers
-app.use(express.static(path.join(__dirname, 'public'), {
-  setHeaders: (res, path) => {
-    if (path.endsWith('.js')) {
-      res.setHeader('Content-Type', 'application/javascript');
-    }
-  }
-}));
+app.use(express.static('public'));
+app.use(express.static('images'));
+app.use(express.static('css'));
+app.use(express.static('js'));
 
-// Example route: Home page
+// Custom middleware to set Content-Type for .js files
+const setJsContentType = (req, res, next) => {
+  if (req.path.endsWith('.js')) {
+    res.setHeader('Content-Type', 'application/javascript');
+  }
+  next();
+};
+
+// Serve static files from 'public' folder with custom headers
+app.use('/public', setJsContentType, express.static(path.join(__dirname, 'public')));
+
+// Serve static files from 'another' folder with custom headers
+app.use('/pacman', setJsContentType, express.static(path.join(__dirname, 'pacman')));
+app.use('/invaders', setJsContentType, express.static(path.join(__dirname, 'invaders')));
+app.use('/pong', setJsContentType, express.static(path.join(__dirname, 'pong')));
+
+
+//public static files
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
 app.get('/pacman', (req, res) => {
   res.sendFile(path.join(__dirname, 'pacman', 'index.html'));
 });
 
-// Example route: About page
+
 app.get('/invaders', (req, res) => {
   res.sendFile(path.join(__dirname, 'invaders', 'index.html'));
 });
 
-// Example route: Contact page
+
 app.get('/pong', (req, res) => {
   res.sendFile(path.join(__dirname, 'pong', 'index.html'));
 });
-
-// Add more routes as needed
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
