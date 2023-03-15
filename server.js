d81a4a1829effd416e89cf41e817624b6490e3e6
Jordan Plows <{ID}+{username}@users.noreply.github.com>
2023-03-14 21:27:06 -0700

    chore: commit save point

--- server.js
+++ server.js
@@ -1,30 +1,23 @@
 const express = require('express');
-const app = express();
+const path = require('path');
 
-// Set the root directory for static files
+const app = express();
+const port = process.env.PORT || 8080;
+
+// public folder
 app.use(express.static(__dirname + '/public'));
 
-// Route to the homepage
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+
+// sendFile will go here
+app.get('/pacman/index.html', function(req, res) {
+  res.sendFile(path.join(__dirname, '/pacman/index.html'));
+});
+app.get('/pong/index.html', function(req, res) {
+  res.sendFile(path.join(__dirname, '/pacman/index.html'));
+});
+app.get('/invaders/index.html', function(req, res) {
+  res.sendFile(path.join(__dirname, '/invaders/index.html'));
 });
 
-// Route to the games page
-app.get('/pacman/index', (req, res) => {
-  res.sendFile(__dirname + 'pacman/index.html');
-});
-
-// Route to the Pacman game page
-app.get('/invaders/index', (req, res) => {
-  res.sendFile(__dirname + 'invaders/index.html');
-});
-
-// Route to the Space Invaders game page
-app.get('/pong/index', (req, res) => {
-  res.sendFile(__dirname + 'pong/index.html');
-});
-
-// Start the server
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
