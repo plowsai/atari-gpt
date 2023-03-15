554e4d041bfe43bc6a094c0a6ae63458447f8e3a
Jordan Plows <{ID}+{username}@users.noreply.github.com>
2023-03-14 19:30:11 -0700

    chore: commit save point

--- server.js
+++ server.js
@@ -1,49 +1,30 @@
-// Define your routes
-const routes = {
-    '/pacman/index.html': 'pacman/index.html',
-    '/pong/index.html': 'pong/index.html',
-    '/invaders': 'invaders/index.html',
-  };
-  
-  // Set up the HTML structure
-  const header = document.querySelector('header');
-  const footer = document.querySelector('footer');
-  const container = document.querySelector('#content');
-  
-  // Load the initial page
-  loadPage(window.location.pathname);
-  
-  // Listen for changes in the browser's URL
-  window.addEventListener('popstate', () => {
-    loadPage(window.location.pathname);
-  });
-  
-  // Function to load a page
-  function loadPage(pathname) {
-    // Get the corresponding HTML file from the routes object
-    const filename = routes[pathname];
-    if (!filename) return;
-  
-    // Load the HTML file and insert it into the container element
-    fetch(filename)
-      .then(response => response.text())
-      .then(html => {
-        header.innerHTML = getHeader(html);
-        container.innerHTML = getContent(html);
-        footer.innerHTML = getFooter(html);
-      });
-  }
-  
-  // Helper functions to extract header, content, and footer from the HTML file
-  function getHeader(html) {
-    return html.match(/<header>([\s\S]*?)<\/header>/i)[0];
-  }
-  
-  function getContent(html) {
-    return html.match(/<main>([\s\S]*?)<\/main>/i)[0];
-  }
-  
-  function getFooter(html) {
-    return html.match(/<footer>([\s\S]*?)<\/footer>/i)[0];
-  }
-  
+const express = require('express');
+const app = express();
+
+// Set the root directory for static files
+app.use(express.static(__dirname + '/public'));
+
+// Route to the homepage
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+// Route to the games page
+app.get('/pacman/index', (req, res) => {
+  res.sendFile(__dirname + '/public/pacman/index.html');
+});
+
+// Route to the Pacman game page
+app.get('/space/index', (req, res) => {
+  res.sendFile(__dirname + '/public/space/index.html');
+});
+
+// Route to the Space Invaders game page
+app.get('/pong/index', (req, res) => {
+  res.sendFile(__dirname + '/public/pong/index.html');
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
